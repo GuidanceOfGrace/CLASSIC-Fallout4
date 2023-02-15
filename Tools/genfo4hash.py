ea163ca04a727ae15c57aa8d273621c2ba65c8a7
Andrew Nelson <evildarkarchon@gmail.com>
2023-02-15 05:49:26 -0800

    Add basic command-line support for hash-generation script.

--- Tools/genfo4hash.py
+++ Tools/genfo4hash.py
@@ -1,8 +1,12 @@
+import argparse
 import hashlib
 from pathlib import Path
 
+parser = argparse.ArgumentParser(description="Generate a hash of the Fallout 4 executable")
+parser.add_argument("path", help="Path to the Fallout 4 executable")
+args = parser.parse_args()
 
-FO4EXE = Path("E:/Games/Steam/steamapps/common/Fallout 4/Fallout4.exe")
+FO4EXE = Path(args.path)
 FO4Hash = hashlib.sha512(FO4EXE.read_bytes()).hexdigest()
 Path("fo4hash.txt").write_text(FO4Hash)
 # print(FO4Hash)
