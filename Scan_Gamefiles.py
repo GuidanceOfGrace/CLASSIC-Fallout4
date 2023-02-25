217da4e374511c674f7b6710b22aa224609ba6fd
Andrew Nelson <evildarkarchon@gmail.com>
2023-02-25 00:06:24 -0800

    Change to MaxStdIO to 2048

--- Scan_Gamefiles.py
+++ Scan_Gamefiles.py
@@ -417,21 +417,16 @@ def scan_mainfiles():
             else:
                 scan_mainfiles_report.append("✔️ Looks Menu (F4EE) parameter in *Buffout4.toml* is correctly configured.\n  -----")
 
-            if BUFF_config["Patches"]["MaxStdIO"] < 8192: # type: ignore
+            if BUFF_config["Patches"]["MaxStdIO"] < 2048: # type: ignore
                 scan_mainfiles_report.extend(["# ❌ WARNING: MaxStdIO parameter value in *Buffout4.toml* might be too low.",
-                                              "Auto-Scanner will increase this value to 8192 to prevent BA2 Limit crashes.",
+                                              "Auto-Scanner will increase this value to 2048 to prevent BA2 Limit crashes.",
                                               "-----"])
-                BUFF_config["Patches"]["MaxStdIO"] = 8192 # type: ignore
-            elif BUFF_config["Patches"]["MaxStdIO"] > 8192: # type: ignore
-                scan_mainfiles_report.extend(["# ❌ WARNING: MaxStdIO parameter value in *Buffout4.toml* might be too high.", # Placeholder message courtesy of Github Copilot
-                                              "Auto-Scanner will decrease this value to 8192 to prevent possible crashes.",
-                                              "-----"])
-                BUFF_config["Patches"]["MaxStdIO"] = 8192 # type: ignore
+                BUFF_config["Patches"]["MaxStdIO"] = 2048 # type: ignore
             elif not isinstance(BUFF_config["Patches"]["MaxStdIO"], int): # type: ignore
-                scan_mainfiles_report.extend(["# ❌ WARNING: MaxStdIO parameter value in *Buffout4.toml* is not a number.", # Another placeholder message courtesy of Github Copilot
-                                              "Auto-Scanner will change this value to 8192 to prevent possible crashes.",
+                scan_mainfiles_report.extend(["# ❌ WARNING: MaxStdIO parameter value in *Buffout4.toml* is not a number.", # Placeholder message courtesy of Github Copilot
+                                              "Auto-Scanner will change this value to 2048.",
                                               "-----"])
-                BUFF_config["Patches"]["MaxStdIO"] = 8192 # type: ignore
+                BUFF_config["Patches"]["MaxStdIO"] = 2048 # type: ignore
             else:
                 scan_mainfiles_report.append("✔️ MaxStdIO parameter value in *Buffout4.toml* is correctly configured.\n  -----")
 
