8d9b28d99f8f0c508a0a8b7536f630722597530d
Andrew Nelson <evildarkarchon@gmail.com>
2023-03-24 04:44:03 -0700

    Add command-line support to CL Full Sort.py

--- CL TOOLS/CL Full Sort.py
+++ CL TOOLS/CL Full Sort.py
@@ -1,23 +1,33 @@
+import argparse
 import os
 import shutil
 from glob import glob
+from pathlib import Path
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("scan_dir", type=str, nargs="?", default=os.getcwd(),
+                        help="Specify the directory to scan for crash logs (default: current directory)")
+    args = parser.parse_args()
+
+    # Initialize variables and print a welcome message
     sort_fail_list = []
     print("Hello World! | Crash Logs Sort | Fallout 4")
 
-    for file in glob("crash-*.log"):
+    # Process each log file and move it to the appropriate directory
+    for file in glob(f"{str(Path(args.scan_dir).resolve())}/crash-*.log"):
         error_num, plugin_idx, check_valid = process_log(file)
 
-        if check_valid == True and plugin_idx and not os.path.exists(error_num):
-            os.mkdir(error_num)
-            shutil.move(file, error_num)
-        elif check_valid == True and plugin_idx:
-            shutil.move(file, error_num)
+        if check_valid and plugin_idx and not os.path.exists(f"{args.scan_dir}/{error_num}"):
+            os.mkdir(f"{args.scan_dir}/{error_num}")
+            shutil.move(file, f"{args.scan_dir}/{error_num}")
+        elif check_valid and plugin_idx:
+            shutil.move(file, f"{args.scan_dir}/{error_num}")
         else:
             sort_fail_list.append(file)
 
+    # Display the list of failed logs and print a completion message
     display_failed_logs(sort_fail_list)
     print("SORTING COMPLETE! Check the newly created folders!")
     os.system("pause")
@@ -28,7 +38,7 @@ def process_log(file):
         all_lines = error_check.readlines()
 
     error_line = all_lines[3]
-    error_num = error_line[-8:].strip()
+    error_num = error_line[-8:].strip()  # Does this need to be changed to accomadate for Buffout 4 NG? - evildarkarchon
 
     plugin_idx, check_valid = find_plugin_index_and_validate(all_lines, error_line)
 
