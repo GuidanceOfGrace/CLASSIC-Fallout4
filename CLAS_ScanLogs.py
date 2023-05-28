2f2e24bf725fe73126f37741d2c8b3e0ef982067
Andrew Nelson <evildarkarchon@gmail.com>
2023-05-28 04:51:28 -0700

    Give the error code regex group a name. This will make it easier to reference should we decide to use it in the future.

--- CLAS_ScanLogs.py
+++ CLAS_ScanLogs.py
@@ -60,7 +60,7 @@ def scan_logs():
     amd_pattern = regx.compile(r'GPU.*AMD', regx.IGNORECASE)
     records_pattern = regx.compile('|'.join(regx.escape(pattern) for pattern in UNIVERSE.Crash_Records_Catch))
     records_exclude_pattern = regx.compile('|'.join(regx.escape(pattern) for pattern in GALAXY.Crash_Records_Exclude))
-    unhandled_exception_pattern = regx.compile(r"Unhandled exception.*(\+.{7})?(.*)", regx.IGNORECASE)
+    unhandled_exception_pattern = regx.compile(r"Unhandled exception.*(?P<error_code>\+.{7})?(.*)", regx.IGNORECASE)
     crash_ver_pattern = regx.compile(r"Buffout 4.* v(\d+\.\d+\.\d+)(.*)", regx.IGNORECASE)
     # =================== HELPER FUNCTIONS ===================
 
