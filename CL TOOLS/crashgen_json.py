d4a152a22ef9792977ed2b3bd0a02286ea303d6a
Andrew Nelson <evildarkarchon@gmail.com>
2023-09-10 17:46:40 -0700

    Revert "Revert "Revert "Revert "Removal for new version.""""
    This reverts commit 42f8a20b4f1ef3539953e023964bb00c454fdacb.

--- CL TOOLS/crashgen_json.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("../crashgen.json", "w", encoding="utf-8", errors="ignore") as f:
-    crashgen = {
-        "OLD": "Buffout 4 v1.28.3",
-        "NEW": "Buffout 4 v1.31.1 Feb 28 2023 00:32:02"
-    }
-    json.dump(crashgen, f, indent=4)
