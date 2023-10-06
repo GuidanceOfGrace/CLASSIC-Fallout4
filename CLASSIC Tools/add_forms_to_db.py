51111ceeb8a8d31be99ebee36aee69d9a5b5d6f9
Andrew Nelson <evildarkarchon@gmail.com>
2023-10-06 05:10:26 -0700

    Raise FileNotFoundError if the file specified on the command line doesn't actually exist.

--- CLASSIC Tools/add_forms_to_db.py
+++ CLASSIC Tools/add_forms_to_db.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import sqlite3
 from init_formids_db import insert
 
@@ -8,6 +9,9 @@ argparser.add_argument("--table", help="The table to add the file to", default="
 argparser.add_argument("--db", help="The database to add the file to", default="../CLASSIC Data/databases/FormIDs.db", nargs=1)
 argparser.add_argument("--verbose", help="Prints out the lines as they are added", action="store_true")
 args = argparser.parse_args()
+
+if not os.path.exists(args.db):
+    raise FileNotFoundError(f"Database {args.db} not found")
 
 conn = sqlite3.connect(args.db)
 c = conn.cursor()
