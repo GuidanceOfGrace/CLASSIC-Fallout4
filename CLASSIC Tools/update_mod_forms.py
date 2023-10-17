23426e02ac42853a775a5dbd8e7f1d88b448ed5b
Andrew Nelson <evildarkarchon@gmail.com>
2023-10-17 03:58:48 -0700

    Looks like things weren't as consistent as I thought.

--- CLASSIC Tools/update_mod_forms.py
+++ CLASSIC Tools/update_mod_forms.py
@@ -12,32 +12,32 @@ args = argparser.parse_args()
 if not os.path.exists(args.db):
     raise FileNotFoundError(f"Database {args.db} not found")
 
-conn = sqlite3.connect(args.db)
-c = conn.cursor()
+
 
 with open(args.file, encoding="utf-8", errors="ignore") as f:
-    if not args.verbose:
-        print(f"Updating database with FormIDs from {args.file} to {args.table}")
-    plugins_deleted = []
-    plugins_announced = []
-    for line in f:
-        line = line.strip()
-        if " | " in line:
-            data = line.split(" | ")
-            if len(data) >= 3:
-                plugin, formid, entry, *extra = data
-                if plugin not in plugins_deleted:
-                    print(f"Deleting {plugin}'s FormIDs from {args.table}")
-                    c.execute(f"delete from {args.table} where plugin = ?", (plugin,))
-                    plugins_deleted.append(plugin)
-                if plugin not in plugins_announced and not args.verbose:
-                    print(f"Adding {plugin}'s FormIDs to {args.table}")
-                    plugins_announced.append(plugin)
-                if args.verbose:
-                    print(f"Adding {line} to {args.table}")
-                c.execute(f'''INSERT INTO {args.table} (plugin, formid, entry) 
+    with sqlite3.connect(args.db) as conn:
+        c = conn.cursor()
+        if not args.verbose:
+            print(f"Updating database with FormIDs from {args.file} to {args.table}")
+        plugins_deleted = []
+        plugins_announced = []
+        for line in f:
+            line = line.strip()
+            if " | " in line:
+                data = line.split(" | ")
+                if len(data) >= 3:
+                    plugin, formid, entry, *extra = data
+                    if plugin not in plugins_deleted:
+                        print(f"Deleting {plugin}'s FormIDs from {args.table}")
+                        c.execute(f"delete from {args.table} where plugin = ?", (plugin,))
+                        plugins_deleted.append(plugin)
+                    if plugin not in plugins_announced and not args.verbose:
+                        print(f"Adding {plugin}'s FormIDs to {args.table}")
+                        plugins_announced.append(plugin)
+                    if args.verbose:
+                        print(f"Adding {line} to {args.table}")
+                    c.execute(f'''INSERT INTO {args.table} (plugin, formid, entry) 
                       VALUES (?, ?, ?)''', (plugin, formid, entry))
-conn.commit()
-print("Optimizing database...")
-c.execute("vacuum")
-conn.close()
+        conn.commit()
+        print("Optimizing database...")
+        c.execute("vacuum")
