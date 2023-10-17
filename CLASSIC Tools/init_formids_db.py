6e5eef7a674ab847726fb785789a53ed101abd43
Andrew Nelson <evildarkarchon@gmail.com>
2023-10-17 03:28:55 -0700

    Remove unnecessary hash check

--- CLASSIC Tools/init_formids_db.py
+++ CLASSIC Tools/init_formids_db.py
@@ -5,31 +5,15 @@ import hashlib
 if os.path.exists("../CLASSIC Data/databases/FormIDs.db"):
     os.remove("../CLASSIC Data/databases/FormIDs.db")
 
-conn = sqlite3.connect("../CLASSIC Data/databases/FormIDs.db")
-c = conn.cursor()
-
-if os.path.exists("../CLASSIC Data/databases/Fallout4 FID Main.txt"):
-    c.execute('''CREATE TABLE IF NOT EXISTS Fallout4 
-              (id INTEGER PRIMARY KEY AUTOINCREMENT,  
-               plugin TEXT, formid TEXT, entry TEXT)''')
-    c.execute("CREATE INDEX IF NOT EXISTS Fallout4_index ON Fallout4(formid, plugin COLLATE nocase);")
-if os.path.exists("../CLASSIC Data/databases/Skyrim FID Main.txt"):
-    c.execute('''CREATE TABLE IF NOT EXISTS Skyrim
-                (id INTEGER PRIMARY KEY AUTOINCREMENT,  
-                 plugin TEXT, formid TEXT, entry TEXT)''')
-    c.execute("CREATE INDEX IF NOT EXISTS Skyrim_index ON Skyrim (formid, plugin COLLATE nocase);")
-if os.path.exists("../CLASSIC Data/databases/Starfield FID Main.txt"):
-    c.execute('''CREATE TABLE IF NOT EXISTS Starfield
-                (id INTEGER PRIMARY KEY AUTOINCREMENT,  
-                 plugin TEXT, formid TEXT, entry TEXT)''')
-    c.execute("CREATE INDEX IF NOT EXISTS Starfield_index ON Starfield (formid, plugin COLLATE nocase);")
-
 def insert(line, table="Fallout4"):
-    if line:
-        if "|" in line and len(line.split(" | ")) >= 3:
-            plugin, formid, entry, *extra = line.split(" | ")  # the *extra is for any extraneous data that might be in the line (Python thinks there are more than 3 items in the list for some reason)
-            c.execute(f'''INSERT INTO {table} (plugin, formid, entry) 
+    with sqlite3.connect("../CLASSIC Data/databases/FormIDs.db") as conn:
+        c = conn.cursor()
+        if line:
+            if "|" in line and len(line.split(" | ")) >= 3:
+                plugin, formid, entry, *extra = line.split(" | ")  # the *extra is for any extraneous data that might be in the line (Python thinks there are more than 3 items in the list for some reason)
+                c.execute(f'''INSERT INTO {table} (plugin, formid, entry) 
                       VALUES (?, ?, ?)''', (plugin, formid, entry))
+        conn.commit()
 
 if os.path.exists("../CLASSIC Data/databases/Fallout4 FID Main.txt"):
     with open("../CLASSIC Data/databases/Fallout4 FID Main.txt", encoding="utf-8", errors="ignore") as f:
@@ -39,13 +23,10 @@ if os.path.exists("../CLASSIC Data/databases/Fallout4 FID Main.txt"):
             insert(line)
 if os.path.exists("../CLASSIC Data/databases/Fallout4 FID Mods.txt"):
     print("Inserting Fallout 4 Mod FormIDs...")
-    with open("../CLASSIC Data/databases/Fallout4 FID Mods.txt", "rb") as f:
-        hash = hashlib.sha256(f.read()).hexdigest()
-    if not hash == "bb9ebacc7b1cf232becbf00a942105348ced8a25d8e556fa7739845985cb2553":
-        with open("../CLASSIC Data/databases/Fallout4 FID Mods.txt", encoding="utf-8", errors="ignore") as f:
-            for line in f:
-                line = line.strip()
-                insert(line)
+    with open("../CLASSIC Data/databases/Fallout4 FID Mods.txt", encoding="utf-8", errors="ignore") as f:
+        for line in f:
+            line = line.strip()
+            insert(line)
 
 if os.path.exists("../CLASSIC Data/databases/Skyrim FID Main.txt"):
     print("Inserting Skyrim Main FormIDs...")
@@ -74,6 +55,3 @@ if os.path.exists("../CLASSIC Data/databases/Starfield FID Mods.txt"):
         for line in f:
             line = line.strip()
             insert(line, "Starfield")
-
-conn.commit()
-conn.close()
